28f7875fdc90b51db0174c4e8adc893c95719e9a
curic <curic@sfu.ca>
2023-07-17 17:07:58 -0700

    Added new July 2023 events and updated date of website

--- src/App.js
+++ src/App.js
@@ -6,12 +6,46 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 
 function App() {
+  const event1Date = new Date(2023, 6, 22);
+  const event2Date = new Date(2023, 6, 29);
+
+  const currentDate = new Date();
+
+  const showEvent1 = currentDate <= event1Date;
+  const showEventBox = currentDate <= event2Date;
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="header">
             <img src={circleIcon} className="header-icon" alt="discord icon" />
             SFU Astronomy Club</h1>
+          {showEventBox && (<div className={"event_box"}>
+            <h4>
+                Upcoming events:
+            </h4>
+              {showEvent1 && (<div className={"flex"}>
+                      <p className={"event_text"}>
+                          Thursday, July 20th:
+                      </p>
+                      <Button className="event_button" variant="primary" href="https://sfu.zoom.us/j/87316377035?pwd=Vk1uNHgwRWF5RzFzUjJzK1NjbXZKQT09">
+                          Click to join Zoom Workshop @ 5:30pm
+                      </Button>
+                  </div>)
+              }
+              <div className={"flex"}>
+                  <p className={"event_text"}>
+                      Friday, July 28th:
+                  </p>
+                  <Button className="event_button" variant="primary" href="https://www.eventbrite.ca/e/679734422957">
+                      RSVP to Astronomy 101: Telescope Demos
+                  </Button>
+              </div>
+            </div>)
+          }
+        <h4>
+            Follow us here:
+        </h4>
         <Button className="red" variant="danger" size="lg" href="https://go.sfss.ca/clubs/63/info">
             Join the Club!
         </Button>
@@ -39,7 +73,7 @@ function App() {
       </header>
 
       <footer className="footer">
-        <p>@ SFU Astronomy Club 2022 | Contact: sfuastro@sfu.ca</p>
+        <p>@ SFU Astronomy Club 2023 | Contact: sfuastro@sfu.ca</p>
       </footer>
         <footer className="footer-sm">
             <p>Mascot drawn by Samuel Antunes Miranda, based on original by Pyralsprite</p>
